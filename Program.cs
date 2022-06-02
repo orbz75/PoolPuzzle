36a3065e13ebc841df719170029dcc00f0a9ecdb
orbz75 <orbz75@live.com>
2022-06-02 15:34:17 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PoolPuzzle
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Q q = new Q(Q.R.Next(2) == 1);
+            while (true)
+            {
+                Console.WriteLine($"{q.N1} {q.Op} {q.N2} = ");
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine("Thanks for playing! Come back again.");
+                    return;
+                }
+                if (q.Check(i))
+                {
+                    Console.WriteLine("Congratulations! You are Right.");
+                    q = new Q(Q.R.Next(2) == 1);
+                }
+                else Console.WriteLine("Wrong! Better Luck Next Time Please Try Again.");
+            }
+
+        }
+    }
+}
